f9a4034fea97b72a92f8cd221ef9d98f04f0559a
Sukriti Shukla <sukritishukla@gmail.com>
2021-05-19 17:40:48 +0100

    Fifth Commit

--- UCDPA2_Sukriti_Final_Project.py
+++ UCDPA2_Sukriti_Final_Project.py
@@ -1,7 +1,7 @@
 
 ### Data Source 1 - Data from Kaggle Public Dataset: Life Expectancy (WHO)
 
-## Step 1 - Importing the necessary libraries -
+## Step 1 - Importing all the basic Python libraries for Data Analysis & Visualization -
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -198,13 +198,14 @@ print('X_test:',len(life_feature_test), 'y_test:',len(life_target_test))
 
 
 ## Step 8 - Performing Machine Learning on life expectancy data using Different Models -
-# Step 1 - Importing the necessary libraries to run the model and compute the Regression metrices for evaluation:
+# Step i) - Importing the necessary libraries to run the model and compute the key Regression metrices for Model evaluation:
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import r2_score
+from sklearn.metrics import mean_absolute_percentage_error
 
-# Step 2 - Creating a Function to run different models by fitting each to training set and making predictions on testing set
-# and visualizing the actual vs. predicted values as a plot:
+# Step ii) - Creating a Function to run different models by fitting each and making predictions on testing set;; computing key metrices
+# visualizing the actual vs. predicted values as a plot:
 # defining a Function model_fit_predict for all models:
 def model_fit_predict(regressor):
     """Function to fit the Model to the training set and predict the Intercept, Coefficients and Regression Metrics for the
@@ -253,6 +254,8 @@ def model_fit_predict(regressor):
     print('Mean Squared Error of Model: {:.2f}'.format(mean_squared_error(life_target_test, life_pred)))
     # computing and printing the R-squared value for the model:
     print('R_squared Score on actual vs. prediction: {:.2f}'.format(r2_score(life_target_test, life_pred)))
+    # computing and printing the Mean Absolute Percentage Error (MAPE) for the model:
+    print('Mean Absolute Percentage Error of Model: {:.2f}'.format(mean_absolute_percentage_error(life_target_test, life_pred)))
 
     # Visualizing the Actual vs. Predicted Life Expectancy values generated from the Model as a plot:
     # setting the figure and axes objects and figure sie for the plot:
@@ -271,6 +274,7 @@ def model_fit_predict(regressor):
     # displaying the plot:
     plt.show()
 
+# Step iii) Calling the model_fit_predict function with each model name as argument -
 # A. Running Linear Regression Model on Life expectancy data:
 # importing the model from the sklearn library:
 from sklearn.linear_model import LinearRegression
@@ -286,7 +290,7 @@ model_fit_predict(Ridge)     # calling the function with Ridge as argument
 # C. Running Lasso Regression Model on Life expectancy data:
 # importing the model from the sklearn library:
 from sklearn.linear_model import Lasso
-# passing the second model as argument to the model_fit_predict function:
+# passing the third model as argument to the model_fit_predict function:
 model_fit_predict(Lasso)     # calling the function with Lasso as argument
 
 # D. Running Decision Tree Regression Model on Life expectancy data:
@@ -314,21 +318,22 @@ print('Mean Absolute Error: ', metrics.mean_absolute_error(life_target_test, dec
 print('Mean Squared Error:', metrics.mean_squared_error(life_target_test, decsn_tree_pred).round(decimals=2))
 print('R_squared Score on actual vs. prediction:',
       metrics.r2_score(life_target_test, decsn_tree_pred).round(decimals=2))
+print('Mean Absolute Percentage Error: : {:.2f}'.format(mean_absolute_percentage_error(life_target_test, decsn_tree_pred)))
 
 # storing each of the computed Regression Metrices in a variable
 RMSE = mean_squared_error(life_target_test, decsn_tree_pred, squared=False).round(decimals=2)
 MAE = mean_absolute_error(life_target_test, decsn_tree_pred).round(decimals=2)
 MSE = mean_squared_error(life_target_test, decsn_tree_pred).round(decimals=2)
 R2 = r2_score(life_target_test, decsn_tree_pred).round(decimals=2)
+MAPE = mean_absolute_percentage_error(life_target_test, decsn_tree_pred).round(decimals=2)
 
 # importing the calculated metrics variables into a DataFrame for further use:
 dec_tree_data = {'Model': [regress2.__class__.__name__], 'Root Mean Squared Error': [RMSE],
-                 'Mean Absolute Error': [MAE],
-                 'Mean Squared Error': [MSE], 'R-squared': [R2]}
+                 'Mean Absolute Error': [MAE],'Mean Squared Error': [MSE],
+                 'R-squared': [R2],'Mean Absolute Percentage Error': [MAPE]}
 
 dec_tree_df = pd.DataFrame(dec_tree_data, columns=['Model', 'Root Mean Squared Error', 'Mean Absolute Error',
-                                                   'Mean Squared Error', 'R-squared'])
-
+                                                   'Mean Squared Error', 'R-squared','Mean Absolute Percentage Error'])
 
 
 # E. Running Random Forest Regression Model on Life expectancy data:
@@ -358,19 +363,21 @@ print('Root Mean Squared Error: {:.2f}'.format(rmse_value))
 print('Mean Absolute Error: {:.2f}'.format(mean_absolute_error(life_target_test, rf_pred)))
 print('Mean Squared Error: {:.2f}'.format(mean_squared_error(life_target_test, rf_pred)))
 print('R_squared Score on actual vs. prediction: {:.2f}'.format(r2_score(life_target_test, rf_pred)))
+print('Mean Absolute Percentage Error: : {:.2f}'.format(mean_absolute_percentage_error(life_target_test, rf_pred)))
 
 # storing each of the computed Regression Metrices in a variable:
 RMSE = mean_squared_error(life_target_test, rf_pred, squared=False).round(decimals=2)
 MAE = mean_absolute_error(life_target_test, rf_pred).round(decimals=2)
 MSE = mean_squared_error(life_target_test, rf_pred).round(decimals=2)
 R2 = r2_score(life_target_test, rf_pred).round(decimals=2)
+MAPE = mean_absolute_percentage_error(life_target_test, rf_pred).round(decimals=2)
 
 rf_data = {'Model': [rf.__class__.__name__], 'Root Mean Squared Error': [RMSE], 'Mean Absolute Error': [MAE],
-           'Mean Squared Error': [MSE], 'R-squared': [R2]}
+           'Mean Squared Error': [MSE], 'R-squared': [R2], 'Mean Absolute Percentage Error': [MAPE]}
 
 # importing the calculated metrics variables into a DataFrame for further use:
 rf_df = pd.DataFrame(rf_data, columns=['Model', 'Root Mean Squared Error', 'Mean Absolute Error',
-                                       'Mean Squared Error', 'R-squared'])
+                                       'Mean Squared Error', 'R-squared','Mean Absolute Percentage Error'])
 
 
 # Visualizing the Actual vs. Predicted Life Expectancy values as Subplots from each of the Tree Models:
@@ -390,7 +397,7 @@ plt.title(regress2.__class__.__name__)   # extracting a class name as a string t
 # adding a legend to the plot:
 plt.legend()
 
-# # plotting the graph at the top for Random Forest Regression Model:
+# # plotting the graph at the bottom for Random Forest Regression Model:
 plt.subplot(2, 1, 2)
 # creating the plot for random forest regression model as a scatter and line plot:
 x_ax = range(len(life_target_test))
@@ -402,3 +409,207 @@ plt.title(rf.__class__.__name__)   # extracting a class name as a string to use 
 plt.legend()
 # displaying the plot:
 plt.show()
+
+
+## Step 9 - Performing Feature Selection on Life Expectancy data -
+# A. Feature Selection (Embedded Method) using Lasso regression (L1 Regularization):
+# importing the necessary libraries from scikit-learn:
+from sklearn.linear_model import LassoCV
+
+# instantiating our regressor for performing Lasso Cross-Validation:
+reg = LassoCV()
+
+# fitting our regressor to the training data:
+reg.fit(life_feature_train,life_target_train)
+
+# printing the best alpha and best score from Lasso CV using the alpha_ and score_ attribute :
+print("Best alpha using LassoCV: %.2f" % reg.alpha_)
+print("Best score using LassoCV: %.2f" % reg.score(life_feature_train, life_target_train)) # Returns the coefficient of determination
+
+# creating a Pandas Series by extracting the coef_ attribute against each feature name as index:
+coef = pd.Series(reg.coef_, index = life_feature.columns)
+
+# printing the most important and least important feature counts:
+print("Lasso picked " + str(sum(coef != 0)) + " features and eliminated the other " +  str(sum(coef == 0)) + " features")
+
+# sorting the coefficients and visualizing them in a plot -
+imp_coef = coef.sort_values()
+# setting the figure size for the plot:
+plt.figure(figsize=(8,5))
+# plotting the coefficient counts as a horizontal bar plot:
+imp_coef.plot(kind = "barh")
+# adding the title and x-axis labels to the plot:
+_ = plt.title("Feature Selection using Lasso Model", fontsize=14)
+_ = plt.xlabel('Coefficients')
+# displaying the plot:
+plt.show()
+
+
+# B. Feature Importance using Random Forest Regressor:
+# Note - we had already run the Random Forest Regression model earlier so using feature_importances_ property here:
+# creating a Pandas Series of important features:
+importances_rf = pd.Series(rf.feature_importances_, index=life_feature.columns)
+
+# sorting the feature Series created using the sort_values function :
+sorted_importances_rf = importances_rf.sort_values()
+
+# visualizing the importance of features as a horizontal bar plot:
+sorted_importances_rf.plot(kind='barh',color='lightgreen')
+# adding a title to the plot:
+plt.title("Feature Importance using Random Forests", fontsize=14)
+# displaying the plot:
+plt.show()
+
+
+
+## Step 10 - Implementing Gradient Boosting Algorithms to Improve Model Performance -
+# A. Boosting through Gradient Boosting for Regression:
+# importing the models and utility functions:
+from sklearn.ensemble import GradientBoostingRegressor
+
+# We have already split the dataset into 70% train and 30% test so do not repeat it.
+
+# instantiating a gradient boosting regressor object:
+# not specifying the subsample parameter defaults it to 1.0 hence, the model performs Gradient Boosting on data
+gbt = GradientBoostingRegressor(n_estimators=400, max_depth=8,random_state=42)
+
+# fitting 'gbt' to the training set of data:
+gbt.fit(life_feature_train, life_target_train)
+
+# predicting the test set labels:
+gbt_pred = gbt.predict(life_feature_test)
+
+# computing and printing the key Regression Metrices after performing Gradient Boosting:
+rmse_test = mean_squared_error(life_target_test, gbt_pred)**(1/2)
+
+print('Regression Metrics:',gbt.__class__.__name__,'\n')
+print('Root Mean Squared Error after Gradient Boosting: {:.2f}'.format(rmse_test))
+print('Mean Absolute Error after Gradient Boosting: {:.2f}'.format(mean_absolute_error(life_target_test, gbt_pred)))
+print('Mean Squared Error after Gradient Boosting: {:.2f}'.format(mean_squared_error(life_target_test, gbt_pred)))
+print('R_squared Score on actual vs. prediction: {:.2f}'.format(r2_score(life_target_test, gbt_pred)))
+print('Mean Absolute Percentage Error after Gradient Boosting: {:.2f}'.format(mean_absolute_percentage_error(life_target_test, gbt_pred)))
+
+
+# B. Boosting through Stochastic Gradient Boosting for Regression:
+# We have already split the dataset into 70% train and 30% test so do not repeat it.
+
+# instantiating a stochastic gradient boosting regressor object:
+# specifying the subsample parameter < 1.0 leads to model performing Stochastic Gradient Boosting on the data
+sgbt = GradientBoostingRegressor(n_estimators=400, max_depth=8, max_features=0.2,subsample=0.8,random_state=42)
+
+# fitting 'sgbt' to the training set of data:
+sgbt.fit(life_feature_train, life_target_train)
+
+# predicting the test set labels:
+sgbt_pred = sgbt.predict(life_feature_test)
+
+# computing and printing the key Regression Metrices after performing Stochastic Gradient Boosting:
+rmse_test = mean_squared_error(life_target_test, sgbt_pred)**(1/2)
+
+print('Regression Metrics: StochasticGradientBoostingRegressor \n')
+print('Root Mean Squared Error after Stochastic Gradient Boosting: {:.2f}'.format(rmse_test))
+print('Mean Absolute Error after Stochastic Gradient Boosting: {:.2f}'.format(mean_absolute_error(life_target_test, sgbt_pred)))
+print('Mean Squared Error after Stochastic Gradient Boosting: {:.2f}'.format(mean_squared_error(life_target_test, sgbt_pred)))
+print('R_squared Score on actual vs. prediction: {:.2f}'.format(r2_score(life_target_test, sgbt_pred)))
+print('Mean Absolute Percentage Error after Gradient Boosting: {:.2f}'.format(mean_absolute_percentage_error(life_target_test, sgbt_pred)))
+
+
+# Visualizing the Actual vs. Predicted Life Expectancy values as Subplots from each of the Gradient Boosting Algorithms:
+# setting up the matplotlib figure for subplots:
+plt.figure(figsize=(18,6))
+# adding a main title for all the subplots:
+plt.suptitle('Visualizing the Actual vs. Predicted Life Expectancy values from the Gradient Boosting Algorithms')
+
+# plotting the graph at the top for Gradient Boosting algorithm:
+plt.subplot(2, 1, 1)
+# creating the plot for Gradient Boosting algorithm as a scatter and line plot:
+x_ax = range(len(life_target_test))
+plt.scatter(x_ax, life_target_test, s=5, color="blue", label="actual")
+plt.plot(x_ax, gbt_pred, lw=0.8, color="red", label="predicted")
+# adding a title to the subplot:
+plt.title(gbt.__class__.__name__)   # extracting a class name as a string to use as plot title
+# adding a legend to the plot:
+plt.legend()
+
+# Plotting the graph at the bottom for Stochastic Gradient Boosting algorithm:
+plt.subplot(2, 1, 2)
+# creating the plot for Stochastic Gradient Boosting algorithm as a scatter and line plot:
+x_ax = range(len(life_target_test))
+plt.scatter(x_ax, life_target_test, s=5, color="blue", label="actual")
+plt.plot(x_ax, sgbt_pred, lw=0.8, color="green", label="predicted")
+#  adding a title to the subplot:
+plt.title('StochasticGradientBoostingRegressor')
+# adding a legend to the plot:
+plt.legend()
+# finally displaying the plot:
+plt.show()
+
+
+## Step 11 - Model Tuning using Grid Search Cross Validation -
+# Step i) - Creating a Function to tune the hyperparameters of different models using GridSearchCV -
+# importing the necessary library to perform GridSearchCV:
+from sklearn.model_selection import GridSearchCV
+
+# defining a Function model_tuning for all the Models -
+def model_tuning(model, param_dict):
+    """Function to tune the hyperparamters of a Model using Grid Search Cross Validation"""
+
+    # inspecting the hyperparameters for the chosen Model:
+    model.get_params()
+
+    # defining the grid or combination of parameters that we want to test out on the chosen Model:
+    model_grid = param_dict
+
+    # instantiating a GridSearchCV object on the Model with scoring parameter set to r-squared and cross-validation folds set to 5:
+    grid_model = GridSearchCV(estimator=model, param_grid=model_grid, cv=5,
+                              scoring='r2')  # scoring means the metric to optimize
+
+    # fitting the grid_model to the training set of data:
+    grid_model.fit(life_feature_train, life_target_train)
+
+    # extracting the best estimator, score and parameters from the model and printing them:
+    print("Results from GridSearchCV on", model.__class__.__name__)
+
+    # printing the estimator which gave highest score (or smallest loss if specified) on the left out data:
+    print("Best Estimator:\n", grid_model.best_estimator_)
+
+    # printing the mean cross-validated score of the best_estimator:
+    print("\n Best Score:\n", grid_model.best_score_)
+
+    # printing the parameter setting that gave the best results on the hold out data:
+    print("\n Best Hyperparameters:\n", grid_model.best_params_)
+
+    # extracting best model from the grid:
+    best_model_grid = grid_model.best_estimator_
+
+    # predicting the test set labels:
+    y_pred = best_model_grid.predict(life_feature_test)
+
+    # Computing & printing the key Regression Metrics after Tuning the model & predicting with the best parameters:
+    rmse_test = mean_squared_error(life_target_test, y_pred) ** (1 / 2)
+
+    print('\n Regression Metrices on Tuned Model:')
+    # computing and printing the Root Mean Squared Error (RMSE) for the model:
+    print('Root Mean Squared Error of Best Model: {:.2f}'.format(rmse_test))
+    # computing and printing the Mean Absolute Error (MAE) for the model:
+    print('Mean Absolute Error of Best Model: {:.2f}'.format(mean_absolute_error(life_target_test, y_pred)))
+    # computing and printing the Mean Squared Error (MSE) for the model:
+    print('Mean Squared Error of Best Model: {:.2f}'.format(mean_squared_error(life_target_test, y_pred)))
+    # computing and printing the R-squared value for the model:
+    print('R_squared Score on actual vs. prediction: {:.2f}'.format(r2_score(life_target_test, y_pred)))
+    # computing and printing the Mean Absolute Percentage Error (MAPE) for the model:
+    print('Mean Absolute Percentage Error of Model: {:.2f}'.format(
+        mean_absolute_percentage_error(life_target_test, y_pred)))
+
+
+# Step ii) Calling the model_tuning function with each model name and hyperparameter grid as arguments -
+# A. Implementing GridSearchCV on Random Forest Regression Model:
+# defining a grid of hyperparameters for Random Forest Regression Model:
+params_rf = {'n_estimators': [300, 400, 500],
+            'max_depth': [1, 4, 8],
+            'min_samples_leaf': [0.1, 0.2],
+            'max_features' : ['log2','sqrt']}
+
+# calling the model_tuning function with requisite Model arguments:
+model_tuning(RandomForestRegressor(random_state=42),params_rf)
+
